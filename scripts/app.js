4b6de33c565cbe87d41eaaf65358f9af7fee3a44
Russel <kotashov03@gmail.com>
2023-05-18 01:18:43 +0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,32 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     
+    //открытие меню
+
+    const burgerMenu = document.querySelector('.burger-btn');
+    const mobileMenu = document.querySelector('.mobile-header');
+
+    function OpenMenu() {
+        burgerMenu.classList.add('active');     
+        mobileMenu.classList.add('active');
+        document.querySelector('main.main').classList.add('black');
+        document.body.style.overflow=  'hidden';
+    }
+    function CloseMenu() {
+        burgerMenu.classList.remove('active');     
+        mobileMenu.classList.remove('active');
+        document.querySelector('main.main').classList.remove('black');
+        document.body.style.overflow = 'auto';
+    }
+    if (burgerMenu) {
+        burgerMenu.addEventListener('click', (e)=>{
+            if (e.target.classList.contains('active')) {
+                CloseMenu();
+            } else {
+                OpenMenu();
+            }
+        })
+    }
+
     // логика формы 
 
     let loginPage = document.querySelector('.login-page');
